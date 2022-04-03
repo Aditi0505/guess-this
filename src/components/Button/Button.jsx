ae4f2c11b0438e068a42470bc05e2b339eb33f04
Aditi0505 <aditi5may1997@gmail.com>
2022-04-03 15:33:27 +0530

    fix: remove button inside link

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const Button = ({ buttonState, route }) => {
   return (
     <li>
-      <Link to={`/${route}`} className="nav-icon">
-        <button className="btn btn-ghost-primary">{buttonState}</button>
+      <Link to={`/${route}`} className="btn btn-ghost-primary">
+        {buttonState}
       </Link>
     </li>
   );
