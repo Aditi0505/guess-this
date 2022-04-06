daccc7d2890ee55db5abe4a2071ed2fcde16a24f
Aditi0505 <aditi5may1997@gmail.com>
2022-04-06 17:56:25 +0530

    feat: add handlers for filter, search and sort

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
+export { getSortedCategories } from "./sorted-categories";
+export { getFilteredCategories } from "./filtered-categories";
 export { fetchNextQuestion } from "./next-question";
 export { colorHandler } from "./color-handler";
 export { playQuiz } from "./play-quiz";
 export { setTitle } from "./set-title";
+export { getSearchedCategories } from "./searched-categories";
