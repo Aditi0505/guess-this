9422d77f1f3f815e644459f88da54e3375a0d9da
Aditi0505 <aditi5may1997@gmail.com>
2022-06-14 15:47:51 +0530

    fix: remove unwanted types

--- src/reducer/filter-reducer.js
+++ src/reducer/filter-reducer.js
@@ -2,8 +2,6 @@ export const filterReducer = (state, { type, payload }) => {
   switch (type) {
     case "SORT":
       return { ...state, sortBy: payload };
-    case "RATING":
-      return { ...state, rating: payload };
     case "FILTER_BY_DIFFICULTY":
       return {
         ...state,
@@ -17,11 +15,6 @@ export const filterReducer = (state, { type, payload }) => {
         medium: false,
         hard: false,
         searchKeyword: "",
-      };
-    case "FILTER_BY_PRICE":
-      return {
-        ...state,
-        price: payload,
       };
     case "FILTER_BY_SEARCH":
       return {
