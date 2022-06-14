9edd59503eb5d394189686c11ef7ebe8c542986f
Aditi0505 <aditi5may1997@gmail.com>
2022-06-14 15:02:04 +0530

    feat:add auth logic

---
+++ src/services/auth-service.js
@@ -0,0 +1,84 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export const loginHandler = async (
+  email,
+  password,
+  authDispatch,
+  navigate,
+  location,
+  toast
+) => {
+  try {
+    if (email !== "" && password !== "") {
+      const authentication = getAuth();
+      const data = await signInWithEmailAndPassword(
+        authentication,
+        email,
+        password
+      );
+
+      authDispatch({
+        type: "LOGIN",
+        payload: { email: email, pasword: password, data },
+      });
+      navigate(location.state?.from?.pathname || "/", {
+        replace: true,
+      });
+      toast.success("User logged in successfully!");
+    } else {
+      toast.warn("Please enter correct details");
+    }
+  } catch (e) {
+    toast.error("Cannot login right now");
+  }
+};
+
+export const signupHandler = async (
+  email,
+  password,
+  authDispatch,
+  navigate,
+  location
+) => {
+  try {
+    const authentication = getAuth();
+    if (email !== "" && password !== "") {
+      const data = await createUserWithEmailAndPassword(
+        authentication,
+        email,
+        password
+      );
+      authDispatch({
+        type: "SIGNUP",
+        payload: { email: email, pasword: password, data },
+      });
+      navigate(location.state?.from?.pathname || "/login", {
+        replace: true,
+      });
+      // toastDispatch({
+      //   type: "SHOW",
+      //   payload: "Signup Successful!",
+      // });
+      // data.errors &&
+      //   toastDispatch({
+      //     type: "SHOW",
+      //     payload: data.errors[0],
+      //   });
+    } else {
+      // toastDispatch({
+      //   type: "SHOW",
+      //   payload: "Please enter correct details.",
+      // });
+    }
+  } catch (error) {
+    console.log(error);
+    // toastDispatch({
+    //   type: "SHOW",
+    //   payload: error,
+    // });
+  }
+};
